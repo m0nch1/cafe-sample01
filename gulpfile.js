ba69726195bf96018198b9aab2a8df4ef31e8f6b
m0nch1 <m0nch1.develop@gmail.com>
2020-09-21 21:09:24 +0900

    fix directory structure and some paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,98 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
 const sass = require("gulp-sass");
-const babel = require("gulp-babel");
 const sourcemaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
-const uglify = require("gulp-uglify-es").default;
 const rename = require("gulp-rename");
+const plumber = require("gulp-plumber");
 const del = require("del");
 const browserSync = require("browser-sync");
 
+// js babel
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify-es").default;
+
+// image compressor
+const imagemin = require("gulp-imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminSvgo = require("imagemin-svgo");
+
+// webpack
 const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
-
-// webpackの設定ファイルの読み込み
 const webpackConfig = require("./webpack.config");
 
-//setting : paths
 const paths = {
   root: "./dist/",
-  pug: "./pug/**/*.pug",
-  html: "./dist/**/*.html",
-  cssSrc: "./sass/**/*.scss",
+  pug: "./src/pug/**/*.pug",
+  scssSrc: "./src/sass/**/*.scss",
   cssDist: "./dist/css/",
-  jsSrc: "./js/**/*.js",
+  jsSrc: "./src/js/**/*.js",
   jsDist: "./dist/js/",
-  imageSrc: "./images/**/*",
+  imageSrc: "./src/images/**/*",
   imageDist: "./dist/images/",
 };
 
-const { watch, series, task, src, dest, parallel } = require("gulp");
+const PATHS = {
+  pug: {
+    src: "./src/pug/**/!(_)*.pug",
+    dest: "./dist",
+  },
+  scss: {
+    src: "./src/scss/**/*.scss",
+    dest: "./dist/css",
+  },
+  js: {
+    src: "./src/js/**/*.js",
+    dest: "./dist/js",
+  },
+  image: {
+    src: "./src/images/**/*",
+    dest: "./dist/images",
+  },
+};
 
-//Pug
-task("pug", function () {
-  return src([paths.pug, "!./pug/**/_*.pug"])
+const { watch, series, src, dest, parallel } = require("gulp");
+
+const errorHandler = (err, stats) => {
+  if (ell || (stats && stats.compilation.error.length > 0)) {
+    const error = err || stats.compilation.errors[0].error;
+    notify.onError({ message: "<%= error.message %>" })(error);
+    this.emit("end");
+  }
+};
+
+const pug2Html = () => {
+  return src(PATHS.pug.src)
+    .pipe(plumber({ errorHandler: errorHandler }))
     .pipe(
       pug({
         pretty: true,
         basedir: "./pug",
       })
     )
-    .pipe(dest(paths.root));
-});
+    .pipe(dest(PATHS.pug.dest));
+};
 
-//Sass
-task("sass", function () {
-  return src(paths.cssSrc)
+const scss2Css = () => {
+  return src(PATHS.scss.src)
+    .pipe(plumber({ errorHandler: errorHandler }))
     .pipe(
       sass({
         outputStyle: "expanded", // Minifyするなら'compressed'
       })
     )
-    .pipe(dest(paths.cssDist));
-});
+    .pipe(dest(PATHS.scss.dest));
+};
 
-//JavaScript(babel transpile and minify)
-task("js", function () {
-  return src(paths.jsSrc)
+const jsBabel = () => {
+  return src(PATHS.js.src)
+    .pipe(plumber({ errorHandler: errorHandler }))
     .pipe(sourcemaps.init())
     .pipe(
       babel({
-        presets: ["@babel/env"],
+        presets: ["@babel/preset-env"],
       })
     )
     .pipe(webpackStream(webpackConfig, webpack))
@@ -71,49 +104,66 @@ task("js", function () {
       })
     )
     .pipe(sourcemaps.write("../maps"))
-    .pipe(dest(paths.jsDist));
-});
+    .pipe(dest(PATHS.js.dest));
+};
 
-//Image File
-task("image", function () {
-  return src(paths.imageSrc).pipe(gulp.dest(paths.imageDist));
-});
+const image = () => {
+  return src(PATHS.image.src)
+    .pipe(plumber({ errorHandler: errorHandler }))
+    .pipe(
+      imagemin(
+        [
+          imageminMozjpeg({
+            quality: 80,
+          }),
+          imageminPngquant(),
+          imageminSvgo({
+            plugins: [
+              {
+                removeViewbox: false,
+              },
+            ],
+          }),
+        ],
+        {
+          verbose: true,
+        }
+      )
+    )
+    .pipe(gulp.dest(PATHS.image.dest));
+};
 
-// browser-sync
-task("browser-sync", () => {
+const browserSyncFunc = () => {
   return browserSync.init({
     server: {
-      baseDir: paths.root,
+      baseDir: "./dist",
     },
     port: 3000,
     reloadOnRestart: true,
   });
-});
+};
 
-// browser-sync reload
-task("reload", (done) => {
+const browserReload = (done) => {
   browserSync.reload();
   done();
-});
+  console.info("Browser reload completed");
+};
 
-//watch
-task("watch", (done) => {
-  watch([paths.cssSrc], series("sass", "reload"));
-  watch([paths.jsSrc], series("js", "reload"));
-  watch([paths.pug], series("pug", "reload"));
-  watch([paths.imageSrc], series("image", "reload"));
+const clean = (done) => {
+  del.sync("./dist/**/*", "！./dist");
   done();
-});
+};
 
-//Clean
-task("clean", function (done) {
-  del.sync("./dist/**", "！./dist");
-  done();
-});
+const watchFiles = () => {
+  watch(paths.pug, series(pug2Html, browserReload));
+  watch(paths.scssSrc, series(scss2Css, browserReload));
+  watch(paths.jsSrc, series(jsBabel, browserReload));
+  watch(paths.imageSrc, series(image, browserReload));
+};
 
-task("build", parallel("pug", "sass", "js", "image", "clean"));
+exports.build = series(parallel(clean, pug2Html, scss2Css, jsBabel, image));
 
-task(
-  "default",
-  parallel("watch", "browser-sync", "pug", "sass", "js", "image", "clean")
+exports.default = series(
+  series(clean, pug2Html, scss2Css, jsBabel, image),
+  parallel(watchFiles, browserSyncFunc)
 );
